3dee52f13447228496084c46ded0b301238f204c
Axel <axel.caro@insa-rennes.fr>
2015-11-23 13:07:29 +0100

    UTesting Human units.

--- SmallWorld/SmallWorld.utest/UnitTestUnit.cs
+++ SmallWorld/SmallWorld.utest/UnitTestUnit.cs
@@ -9,6 +9,7 @@ namespace SmallWorld.utest
     [TestClass]
     public class UnitTestUnit
     {
+        // TESTING ELVEN UNITS //
         [TestMethod]
         public void TestElfCrossingWater()
         {
@@ -114,6 +115,96 @@ namespace SmallWorld.utest
 
             unit.getMoveCost(water);
         }
+
+
+        // TESTING HUMAN UNITS //
+        [TestMethod]
+        public void TestHumanCrossingAll()
+        {
+            HumanUnit unit = new HumanUnit();
+            Mountain mountain = new Mountain();
+            Forest forest = new Forest();
+            Plain plain = new Plain();
+            Water water = new Water();
+            bool res = unit.canCrossTile(mountain);
+
+            res = res && unit.canCrossTile(forest);
+            res = res && unit.canCrossTile(plain);
+            res = res && unit.canCrossTile(water);
+
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestHumanCountPointsForest()
+        {
+            HumanUnit unit = new HumanUnit();
+            Forest forest = new Forest();
+
+            Assert.AreEqual(1, unit.countPoints(forest));
+        }
+
+        [TestMethod]
+        public void TestHumanCountPointsMountain()
+        {
+            HumanUnit unit = new HumanUnit();
+            Mountain mountain = new Mountain();
+
+            Assert.AreEqual(1, unit.countPoints(mountain));
+        }
+
+        [TestMethod]
+        public void TestHumanCountPointsPlain()
+        {
+            HumanUnit unit = new HumanUnit();
+            Plain plain = new Plain();
+
+            Assert.AreEqual(2, unit.countPoints(plain));
+        }
+
+        [TestMethod]
+        public void TestHumanCountPointsWater()
+        {
+            HumanUnit unit = new HumanUnit();
+            Water water = new Water();
+
+            Assert.AreEqual(0, unit.countPoints(water));
+        }
+
+        [TestMethod]
+        public void TestHumanGetAttackRange()
+        {
+            HumanUnit unit = new HumanUnit();
+            Mountain mountain = new Mountain();
+            Forest forest = new Forest();
+            Plain plain = new Plain();
+            Water water = new Water();
+            bool res = true && unit.getAttackRange(mountain) == 1;
+
+            res = res && unit.getAttackRange(forest) == 1;
+            res = res && unit.getAttackRange(plain) == 1;
+            res = res && unit.getAttackRange(water) == 1;
+
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestHumanMoveCostAll()
+        {
+            HumanUnit unit = new HumanUnit();
+            Mountain mountain = new Mountain();
+            Forest forest = new Forest();
+            Plain plain = new Plain();
+            Water water = new Water();
+            bool res = true && unit.getMoveCost(mountain) == 1;
+
+            res = res && unit.getMoveCost(forest) == 1;
+            res = res && unit.getMoveCost(plain) == 1;
+            res = res && unit.getMoveCost(water) == 1;
+
+            Assert.IsTrue(res);
+        }
+
 
     }
 }
