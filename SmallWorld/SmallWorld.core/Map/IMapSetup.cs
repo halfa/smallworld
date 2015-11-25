824801d74f8a6e16dae8e7ca39435b18bb21aead
Axel <axel.caro@insa-rennes.fr>
2015-11-25 11:25:57 +0100

    Documenting map setup.

--- SmallWorld/SmallWorld.core/Map/IMapSetup.cs
+++ SmallWorld/SmallWorld.core/Map/IMapSetup.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace SmallWorld.Core
 {
+    /// <summary>
+    /// This interface is a template for Map Setups.
+    /// </summary>
     public interface IMapSetup
     {
+        /// <summary>
+        /// Reand and write access to the map field (the map to set up).
+        /// </summary>
         Map map { get; set; }
 
+        /// <summary>
+        /// This method should be implemented by all sub-classes, and set up the map.
+        /// </summary>
         void setupMap();
     }
 }
