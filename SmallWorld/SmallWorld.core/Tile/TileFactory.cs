797195ec073b2eb7bd7df58ca3f3ca9b650d6fb1
Axel <axel.caro@insa-rennes.fr>
2015-11-28 10:34:16 +0100

    Cleaning System dependencies.

--- SmallWorld/SmallWorld.core/Tile/TileFactory.cs
+++ SmallWorld/SmallWorld.core/Tile/TileFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SmallWorld.Core
+﻿namespace SmallWorld.Core
 {
     /// <summary>
     /// This class is a template for a flyweight tile factory.
