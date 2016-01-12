aab83703387ffc005399dca20789f08bb51d64bf
Maximilien Richer <youngyukari@gmail.com>
2016-01-12 21:33:09 +0100

    Maps : Fixup for win32 generation

--- SmallWorld/SmallWorld.lib/Algo.cpp
+++ SmallWorld/SmallWorld.lib/Algo.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 void Algo::fillMap(TileType map[], int size)
 {
-	fillMapWithNoise(map, size, 0);
+	fillMapWithNoise(map, size);
 }
 
 void Algo::fillMapByPermutations(TileType map[], int size)
@@ -36,47 +36,47 @@ void Algo::fillMapByPermutations(TileType map[], int size)
 */
 void Algo::fillMapWithNoise(TileType map[], int size)
 {
-	int height = (int) sqrt(size);
+	int height = (int)sqrt(size);
 	// initialise random generator
-    random_device rd;
-    int seed = rd();
-	
-    mt19937 generator(seed);
-    uniform_real_distribution<double> dis(0.0, 1.0);
-    uniform_real_distribution<double> translate(-100000, 100000);
-    
-    double zseed = dis(generator);
-    int tx = round(translate(generator));
-    int ty = round(translate(generator));
-    
-    // generate a mask for Perlin
-    PerlinNoise pn;
+	random_device rd;
+	int seed = rd();
+
+	mt19937 generator(seed);
+	uniform_real_distribution<double> dis(0.0, 1.0);
+	uniform_real_distribution<double> translate(-100000, 100000);
+
+	double zseed = dis(generator);
+	int tx = round(translate(generator));
+	int ty = round(translate(generator));
+
+	// generate a mask for Perlin
+	PerlinNoise pn;
 	vector<float> mask(size);
-    for (int x = 0; x < height; x++)
-		for (int y = 0; y < height; y++)
-		    mask[height*y+x] =  pn.noise(x+tx, y+ty, zseed);
-
-    /* Normalize mask
-     * From [min, max] to [0, 3.99]
-     * 0-.99  -> 0
-     * 1-1.99 -> 1
-     * 2-2.99 -> 2
-     * 3-3.99 -> 3
-     */
-    vector<float>::iterator maxit = max_element(mask.begin(), mask.end());
-    vector<float>::iterator minit = min_element(mask.begin(), mask.end());
-    float max = *maxit - *minit; // prepare max for normalisation
-    float min = *minit;
-    
-    // normalize the mask vector to [0;4[
-    vector<float>::iterator i;
-    for(i = mask.begin(); i < mask.end(); i++)
-        (*i) = ( ((*i)-min)/max ) * 3.99;
-
-    // fill the map with tiles
 	for (int x = 0; x < height; x++)
 		for (int y = 0; y < height; y++)
-			map[height*y + x] = (TileType)(mask[height*y+x]);
+			mask[height*y + x] = pn.noise(x + tx, y + ty, zseed);
+
+	/* Normalize mask
+	 * From [min, max] to [0, 3.99]
+	 * 0-.99  -> 0
+	 * 1-1.99 -> 1
+	 * 2-2.99 -> 2
+	 * 3-3.99 -> 3
+	 */
+	vector<float>::iterator maxit = max_element(mask.begin(), mask.end());
+	vector<float>::iterator minit = min_element(mask.begin(), mask.end());
+	float max = *maxit - *minit; // prepare max for normalisation
+	float min = *minit;
+
+	// normalize the mask vector to [0;4[
+	vector<float>::iterator i;
+	for (i = mask.begin(); i < mask.end(); i++)
+		(*i) = (((*i) - min) / max) * 3.99;
+
+	// fill the map with tiles
+	for (int x = 0; x < height; x++)
+		for (int y = 0; y < height; y++)
+			map[height*y + x] = (TileType)((int)mask[height*y + x]);
 }
 
 int Algo::suggestMove(int points[], int nbChoice, int suggestions[])
